3489a34bf5e5d56fd295ebb3911a6a6b8f10156a
Ahmadshoh Nasrullozoda <ahmadshoh.nasrullozoda@quimron.com>
2023-07-03 10:18:46 +0200

    fixed bug with ApplicationHelper

--- lib/rails_accordion.rb
+++ lib/rails_accordion.rb
@@ -7,8 +7,26 @@ loader.ignore("#{__dir__}/generators")
 loader.setup
 
 module RailsAccordion
+  class Railtie < Rails::Railtie
+    ActiveSupport.on_load :action_view do
+      include RailsAccordion
+    end
+  end
+
   def self.root
     RailsAccordion::Engine.routes.find_script_name({})
+  end
+
+  def accordion(**args, &block)
+    render AccordionComponent.new(**args), &block
+  end
+
+  def accordion_item(**args, &block)
+    render ItemComponent.new(**args), &block
+  end
+
+  def rails_accordion_assets
+    render "rails_accordion/assets"
   end
 end
 
--- lib/rails_accordion/engine.rb
+++ lib/rails_accordion/engine.rb
@@ -2,12 +2,6 @@ module RailsAccordion
   class Engine < ::Rails::Engine
     isolate_namespace RailsAccordion
 
-    initializer "rails_accordion.helpers" do
-      ActiveSupport.on_load :action_controller do
-        helper RailsAccordion::ApplicationHelper
-      end
-    end
-
     config.app_middleware.use(
       Rack::Static,
       urls: ["/rails_accordion_assets"],
